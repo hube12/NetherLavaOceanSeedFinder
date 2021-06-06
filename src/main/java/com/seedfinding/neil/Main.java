ef8f18c7a330a73fd6cd22ee73136517d9f9024e
Neil <h66r10@gmail.com>
2021-06-06 21:47:13 +0200

    make it random

--- src/main/java/com/seedfinding/neil/Main.java
+++ src/main/java/com/seedfinding/neil/Main.java
@@ -14,6 +14,7 @@ import kaptainwutax.mcutils.util.pos.RPos;
 import kaptainwutax.mcutils.version.MCVersion;
 import kaptainwutax.terrainutils.ChunkGenerator;
 
+import java.util.Random;
 import java.util.Scanner;
 import java.util.function.Predicate;
 import java.util.stream.LongStream;
@@ -58,6 +59,7 @@ public class Main {
 
 
 	public static void kernel(long structureSeed) {
+		structureSeed=(new Random()).nextLong();
 		BiomeSource biomeSource = BiomeSource.of(Dimension.NETHER, VERSION, structureSeed);
 		ChunkGenerator generator = ChunkGenerator.of(Dimension.NETHER, biomeSource);
 		Pair<BPos[], Integer> fortressPos = closeFortresses(QUADS, structureSeed);
